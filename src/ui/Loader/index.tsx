8106a599d10e925a15d9d4bf90708e9d78e2dd7a
Stepankc <i21s692@students.it-college.ru>
2024-02-10 15:47:36 +0500

    feat: add loader component

---
+++ src/ui/Loader/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.scss';
+
+const Loader: React.FC = () => {
+  return (
+    <>
+      <div className='page-loader'>
+        <div className="loader-container">
+          <span className="loader"></span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
