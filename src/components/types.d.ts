d18647d2e7d4ca8a8f0467e13f908ecb09d7dda5
Stepankc <i21s692@students.it-college.ru>
2024-02-10 14:48:41 +0500

    feat: add types

---
+++ src/components/types.d.ts
@@ -0,0 +1,20 @@
+interface character {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: 'male' | 'female' | 'n/a';
+}
+
+interface characterProps extends character {
+  image: string;
+}
+
+interface UnsplashResult {
+  urls: {
+    regular: string;
+  };
+}
