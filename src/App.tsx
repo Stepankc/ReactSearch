8f5bfe26d91a61e9d1b90ea9c38cb84548b89ffd
Stepankc <i21s692@students.it-college.ru>
2024-02-10 14:49:01 +0500

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import './assets/global.scss';
+import API from './api/Api';
+import CardList from './components/CardList';
 
 const App: FC = () => {
+  const [charactersData, setCharactersData] = useState<characterProps[]>([]);
+
+  useEffect(() => {
+    const fetchCharactersData = async () => {
+      try {
+        const apiResponse = await API.getCharactersByName('sky');
+        setCharactersData(apiResponse);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchCharactersData();
+  }, []);
+
   return (
     <div>
+      <CardList characters={charactersData} />
     </div>
   );
 };
